c0d1f2a874c1845c8c074f4377cd32240c8748fd
Hakim Ghods <ghods.hakim@hotmail.com>
2020-02-25 04:54:26 +0330

    Add project files.

---
+++ MyClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using Grpc.Net.Client;
+using MyServer;
+using System;
+using System.Threading.Tasks;
+using static MyServer.Numerics;
+
+namespace MyClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new MyServer.Numerics.NumericsClient(channel);
+
+
+            await ClientStreamingCallExample(client);
+
+            Console.ReadLine();
+
+        }
+
+         static Random RNG = new Random();
+
+
+        private static async Task ClientStreamingCallExample(NumericsClient client)
+        {
+            using (var call = client.SendNumber())
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    var number = RNG.Next(5);
+                    Console.WriteLine($"Sending {number}");
+                    await call.RequestStream.WriteAsync(new NumberRequest { Value = number });
+                    await Task.Delay(1000);
+                }
+
+                await call.RequestStream.CompleteAsync();
+
+                var response = await call;
+                Console.WriteLine($"Result: {response.Result}");
+            }
+        }
+    }
+}
---
+++ MyServer/Services/NumberService.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace MyServer
+{
+    public class NumbersService : Numerics.NumericsBase
+    {
+        private readonly ILogger<NumbersService> _logger;
+        public NumbersService(ILogger<NumbersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task<NumberResponse> SendNumber(IAsyncStreamReader<NumberRequest> requestStream, ServerCallContext context)
+        {
+
+            await foreach (var number in requestStream.ReadAllAsync())
+            {
+                _logger.LogInformation($"Incrementing count by {number.Value}");
+                
+            }
+
+            return new NumberResponse { Result = true };
+        }      
+
+
+    }
+}
