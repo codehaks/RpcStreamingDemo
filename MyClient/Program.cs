342997cc55c86d1e499eb8aa83c04f249433590b
Hakim Ghods <ghods.hakim@hotmail.com>
2020-03-06 04:15:59 +0330

    fix

--- MyClient/Program.cs
+++ MyClient/Program.cs
@@ -1,6 +1,9 @@
-﻿using Grpc.Net.Client;
+﻿using Google.Protobuf;
+using Grpc.Net.Client;
 using MyServer;
 using System;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using static MyServer.Numerics;
 
@@ -17,31 +20,62 @@ namespace MyClient
             var client = new MyServer.Numerics.NumericsClient(channel);
 
 
-            await StreamNumbersFromClientToServer(client);
+            await StreamFile(client, @"d:\storm.jpg");
 
             Console.ReadLine();
 
         }
 
-        static Random RNG = new Random();
-
-        private static async Task StreamNumbersFromClientToServer(NumericsClient client)
+        private static async Task StreamFile(NumericsClient client, string filePath)
         {
-            using (var call = client.SendNumber())
+            byte[] buffer;
+            FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            try
             {
-                for (var i = 0; i < 10; i++)
-                {
-                    var number = RNG.Next(5);
-                    Console.WriteLine($"Sending {number}");
-                    await call.RequestStream.WriteAsync(new NumberRequest { Value = number });
-                    await Task.Delay(1000);
-                }
+                int length = (int)fileStream.Length;  // get file length
+                buffer = new byte[length];            // create buffer
+                int count;                            // actual number of bytes read
+                int sum = 0;                          // total number of bytes read
 
-                await call.RequestStream.CompleteAsync();
-
-                var response = await call;
-                Console.WriteLine($"Result: {response.Result}");
+                // read until Read method returns 0 (end of the stream has been reached)
+                while ((count = fileStream.Read(buffer, sum, length - sum)) > 0)
+                    sum += count;  // sum is a buffer offset for next reading
             }
+            finally
+            {
+                fileStream.Close();
+            }
+
+            client.SendFile(new Chunk
+            {
+                Content = ByteString.CopyFrom(buffer)
+            });
+
+            //await call.RequestStream.WriteAsync(new Chunk { Content = ByteString.CopyFrom(buffer)});
+
+            //await call.RequestStream.CompleteAsync();
         }
+
+
+        //static Random RNG = new Random();
+
+        //private static async Task StreamNumbersFromClientToServer(NumericsClient client)
+        //{
+        //    using (var call = client.SendNumber())
+        //    {
+        //        for (var i = 0; i < 10; i++)
+        //        {
+        //            var number = RNG.Next(5);
+        //            Console.WriteLine($"Sending {number}");
+        //            await call.RequestStream.WriteAsync(new NumberRequest { Value = number });
+        //            await Task.Delay(1000);
+        //        }
+
+        //        await call.RequestStream.CompleteAsync();
+
+        //        var response = await call;
+        //        Console.WriteLine($"Result: {response.Result}");
+        //    }
+        //}
     }
 }
--- MyServer/Services/NumberService.cs
+++ MyServer/Services/NumberService.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace MyServer
@@ -11,9 +13,11 @@ namespace MyServer
     public class NumbersService : Numerics.NumericsBase
     {
         private readonly ILogger<NumbersService> _logger;
-        public NumbersService(ILogger<NumbersService> logger)
+        private readonly IWebHostEnvironment _webenv;
+        public NumbersService(ILogger<NumbersService> logger, IWebHostEnvironment webenv)
         {
             _logger = logger;
+            _webenv = webenv;
         }
 
         public override async Task<NumberResponse> SendNumber(IAsyncStreamReader<NumberRequest> requestStream, ServerCallContext context)
@@ -29,6 +33,15 @@ namespace MyServer
 
             return new NumberResponse { Result = total };
         }
+
+        public override async Task<SendResult> SendFile(Chunk request, ServerCallContext context)
+        {
+            var content = request.Content.ToArray();
+            await System.IO.File.WriteAllBytesAsync(_webenv.ContentRootPath + "/Files" + "storm1.jpg", content);
+            return new SendResult { Success = true };
+            //return base.SendFile(request, context);
+        }
+
 
 
     }
